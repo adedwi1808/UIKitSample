a30f764e9069cb2c6cbd79211444fc0518830e3c
Ade Dwi Prayitno <adedwip1808@email.com>
2024-02-12 20:28:53 +0700

    Add TableView

--- UIKitSample/ViewController.swift
+++ UIKitSample/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,
+                      UITableViewDelegate,
+                      UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var sampleData: [String: [String]] = [
+        "Straw Hat Pirates": ["Luffy", "Zoro", "Nami", "Sanji", "Chopper", "Robin", "Franky", "Brook", "Jinbe"],
+        "Red Hair Pirates": ["Shanks", "Benn Beckman", "Yasopp", "Lucky Roux", "Rockstar"],
+        "Whitebeard Pirates": ["Edward Newgate", "Marco", "Jozu", "Vista", "Ace", "Thatch", "Blamenco", "Rakuyo", "Namur", "Blenheim", "Curiel", "Haruta", "Atmos", "Fossa", "Izo", "Squard"],
+        "Blackbeard Pirates": ["Marshall D. Teach (Blackbeard)", "Jesus Burgess", "Van Augur", "Doc Q", "Laffitte", "Shiliew", "Sanjuan Wolf", "Vasco Shot", "Catarina Devon", "Avalo Pizarro"]
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        sampleData.keys.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let pirateGroup = Array(sampleData.keys)[section]
+        let pirates = sampleData[pirateGroup] ?? []
+        return pirates.count
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Pirate", for: indexPath)
+        
+        let pirateGroup = Array(sampleData.keys)[indexPath.section]
+        let pirates = sampleData[pirateGroup] ?? []
+        let pirateName = pirates[indexPath.row]
+        
+        cell.textLabel?.text = pirateName
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        Array(sampleData.keys)[section]
+    }
 }
 
